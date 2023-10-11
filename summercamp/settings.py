c60538543cf616eee625be8314aa52381b7ffdfa
Ujwal Ramesh Karajgikar <143509151+ujwalrk@users.noreply.github.com>
2023-10-11 16:16:25 +0530

    Update settings.py

--- summercamp/settings.py
+++ summercamp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5kj5_)16g-^op_bcyh^ywytge@@s*h1^8uk2d+f#k5t@dw)hu!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'raghuck.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'raghuck.pythonanywhere.com', '35.154.78.51']
 
 
 # Application definition
